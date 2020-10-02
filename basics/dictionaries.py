ed675aac51333c4430967c6ae9bc0f69f9704913
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2020-10-02 17:52:02 +0530

    Update dictionaries.py

--- basics/dictionaries.py
+++ basics/dictionaries.py
@@ -2,6 +2,10 @@
 purse = dict()
 purse['money'] = 12
 
+# create a dictionary in another way
+d1 = {'apple': 'green','banana':'yellow'}
+print(d1)# prints items present in dictionary d1
+
 # you dont have to append, just define a new key value
 purse['candy'] = 3
 purse['tissues'] = 75
@@ -10,8 +14,18 @@ purse['tissues'] = 75
 print(purse)
 print(purse['candy'])
 
+#replicate of current dictonary
+purse1 = purse.copy() # copies all current data present in purse to purse1 dictionary
+print(purse1)
+
+#print key values of purse
+print(purse.keys())
 
 # update the dictonary value
 purse['candy'] = purse['candy'] + 2
 print(purse['candy'])
+
+# Update dictionary in another way
+d1.update({'mango': 'yellow'})
+print(d1)#prints updated d1
 
